3709a1431a403583bec40949ff4005bf232ac79e
Giuseppe Barbieri <elect86@gmail.com>
2021-11-04 09:24:04 +0100

    attempting filtering out fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ description = "QuPath extension to use Warpy"
 
 group = "ch.epfl.biop"
 // artifact = "qupath-extension-warpy"
-version = "0.1.1.1"
+version = "0.1.1.3"
 
 dependencies {
     val qupathVersion = "0.3.0" // For now
@@ -88,7 +88,8 @@ tasks.test { useJUnitPlatform() }
 
 publishing {
     publications.create<MavenPublication>("maven") {
-        from(components.getByName("java"))
+//        from(components.getByName("java"))
+        artifacts.removeIf { it.classifier == "all" }
     }
     repositories {
         maven {
