84545ba2a8debcf40a3e81ace97a59750af25255
phaub <peterhaub@web.de>
2021-12-17 13:00:43 +0100

    Update ImageCombinerWarpyExtension.java

--- src/main/java/qupath/ext/imagecombinerwarpy/ImageCombinerWarpyExtension.java
+++ src/main/java/qupath/ext/imagecombinerwarpy/ImageCombinerWarpyExtension.java
@@ -24,7 +24,7 @@
  * 
  * .. a QuPath extension based on the QuPath 'Interactive Image Alignment' tool
  *  
- * The ImageCombinerWarpy is thought as an experimental² tool.
+ * The ImageCombinerWarpy is thought as an experimentalÂ² tool.
  * 
  * It is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
@@ -148,7 +148,7 @@ public class ImageCombinerWarpyExtension implements QuPathExtension, GitHubProje
 
 	@Override
 	public GitHubRepo getRepository() {
-		return GitHubRepo.create(getName(), "qupath", "qupath-extension-imagecombinerwarpy");
+		return GitHubRepo.create(getName(), "biop", "qupath-extension-warpy");
 	}
 
 	@Override
