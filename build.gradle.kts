c6834279992ad7f6556558fda9a0e36a6805aa6d
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-12-22 11:30:06 +0100

    bump to v0.2.3-snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ description = "QuPath extension to use Warpy"
 
 group = "ch.epfl.biop"
 // artifact = "qupath-extension-warpy"
-version = "0.2.2-SNAPSHOT"
+version = "0.2.3-SNAPSHOT"
 
 dependencies {
     val qupathVersion = "0.3.0" // For now
