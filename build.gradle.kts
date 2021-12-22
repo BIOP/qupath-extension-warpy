b9f1f8c0748f1c7b044f720fb30d915d4b33fb81
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-12-22 10:39:01 +0100

    attempt release 0.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ description = "QuPath extension to use Warpy"
 
 group = "ch.epfl.biop"
 // artifact = "qupath-extension-warpy"
-version = "0.2.1-SNAPSHOT"
+version = "0.2.1"
 
 dependencies {
     val qupathVersion = "0.3.0" // For now
