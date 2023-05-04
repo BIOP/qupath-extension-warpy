2a91f9ee5b54a6a07b5fb3a80868ecffdd539987
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2023-05-04 17:15:41 +0200

    bumps to next dev sequence

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ description = "QuPath extension to use Warpy"
 
 group = "ch.epfl.biop"
 // artifact = "qupath-extension-warpy"
-version = "0.2.4"
+version = "0.2.5-SNAPSHOT"
 
 dependencies {
     val qupathVersion = "0.4.3" // For now
