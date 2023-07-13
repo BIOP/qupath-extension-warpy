cf7a3d22487904fb1b4699daa20c55de7ed75406
oburri <olivier.burri@gmail.com>
2023-07-13 13:18:36 +0200

    bumps version

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ description = "QuPath extension to use Warpy"
 
 group = "ch.epfl.biop"
 // artifact = "qupath-extension-warpy"
-version = "0.2.5-SNAPSHOT"
+version = "0.2.5"
 
 dependencies {
     val qupathVersion = "0.4.3" // For now
