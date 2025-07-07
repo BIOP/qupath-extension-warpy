68f759dc5f899feb2c3843cbe15ea7bd0c972a86
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2025-07-07 15:16:58 +0200

    adds a publishing action

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("com.gradleup.shadow") version "8.3.5"
     // QuPath Gradle extension convention plugin
     id("qupath-conventions")
+    // Add the maven-publish plugin
+    id("maven-publish")
 }
 
 // TODO: Configure your extension here (please change the defaults!)
@@ -21,4 +23,65 @@ dependencies {
     implementation("commons-io:commons-io:2.15.0")
     implementation("net.imglib2:imglib2-realtransform:3.1.2")
 
+}
+
+// Publishing configuration for SciJava Maven
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set("Warpy QuPath Extension")
+                description.set("QuPath extension that supports spline transformations.")
+                url.set("https://github.com/BIOP/qupath-extension-warpy")
+
+                licenses {
+                    license {
+                        name.set("BSD-3-Clause")
+                        url.set("https://opensource.org/licenses/BSD-3-Clause")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("NicoKiaru")
+                        name.set("Nicolas Chiaruttini")
+                        email.set("nicolas.chiaruttini@epfl.ch")
+                    }
+                    developer {
+                        id.set("iwbh15")
+                        name.set("Peter Haub")
+                        email.set("peterhaub@web.de")
+                    }
+                    developer {
+                        id.set("lacan")
+                        name.set("Olivier Burri")
+                        email.set("olivier.burri@epfl.ch")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/BIOP/qupath-extension-warpy.git")
+                    developerConnection.set("scm:git:ssh://github.com:BIOP/qupath-extension-warpy.git")
+                    url.set("https://github.com/BIOP/qupath-extension-warpy")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "scijava"
+            url = if (version.toString().endsWith("SNAPSHOT")) {
+                uri("https://maven.scijava.org/content/repositories/snapshots")
+            } else {
+                uri("https://maven.scijava.org/content/repositories/releases")
+            }
+            credentials {
+                username = System.getenv("MAVEN_USER")
+                password = System.getenv("MAVEN_PASS")
+            }
+        }
+    }
 }
