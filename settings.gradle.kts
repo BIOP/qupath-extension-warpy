414eb3884695e077911549b54d2850b0ab522e29
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2025-07-07 13:41:52 +0200

    check if Claude is right

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,10 @@ pluginManagement {
     }
 }
 
-qupath {
-    version = "0.6.0"
-}
-
-// Apply QuPath Gradle settings plugin to handle configuration
 plugins {
     id("io.github.qupath.qupath-extension-settings") version "0.2.1"
+}
+
+qupath {
+    version = "0.6.0"
 }
